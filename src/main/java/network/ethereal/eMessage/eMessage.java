ec7adae9f13bc3c4e8279b198b915a05c64ee4a7
Justin T. Brown <Grasinga@gmail.com>
2015-07-23 16:23:01 -0600

    Comments & JAR

--- src/main/java/network/ethereal/eMessage/eMessage.java
+++ src/main/java/network/ethereal/eMessage/eMessage.java
@@ -5,15 +5,18 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+// Main class
 public class eMessage extends JavaPlugin
 {
 	Logger log = Logger.getLogger("Minecraft");
-	  
+	
 	public void onEnable() {
 		this.log.info("eMessage Enabled!");
 		
+		// Allows the use of the /emsg command and passes in eMessage
 		getCommand("emsg").setExecutor(new eMessageCommands(this));
 		
+		// Sends /emsg command to console to start the advertising.
 		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "emsg");
 	}   
 	
--- src/main/java/network/ethereal/eMessage/eMessageCommands.java
+++ src/main/java/network/ethereal/eMessage/eMessageCommands.java
@@ -6,19 +6,24 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import io.puharesource.mc.titlemanager.api.ActionbarTitleObject;
+import io.puharesource.mc.titlemanager.api.ActionbarTitleObject; // Import from TitleManager
 
 public class eMessageCommands implements CommandExecutor {
 	
+	// Initialize plugin variable
 	private JavaPlugin plugin = null;
+	
+	 // Variable used to change between messages
 	private int msg = 1;
 	
+	// Constructor sets the plugin variable to eMessage
 	public eMessageCommands(JavaPlugin p){plugin = p;}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] extras) {
 		if (label.equalsIgnoreCase("emsg")){
 			if(extras.length <= 0){
+				// /emsg creates a recurring advertisement. The text of the advertisement is based on the variable msg.
 				 Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
 		            @Override
 		            public void run() {
@@ -39,17 +44,19 @@ public class eMessageCommands implements CommandExecutor {
 		            	}
 			            		
 			        }
-			     }, 0L, 375L);
+			     }, 0L, 375L); // 375 = Number of in-game ticks before next message appears.
 				 return true;
 			}
+			// Reload command /emsg reload
 			else if(extras.length == 1 && extras[0].equalsIgnoreCase("reload")) {
 				try{
 				 Bukkit.getServer().getPluginManager().disablePlugin(plugin);
 				 Bukkit.getServer().getPluginManager().enablePlugin(plugin);
 				 sender.sendMessage(ChatColor.GREEN + "eMessage has been reloaded!");
-				}catch(Exception e){sender.sendMessage("Error: " + e); return false;}
+				}catch(Exception e){sender.sendMessage("Error: " + e); return false;} // Fails to reload.
 				return true;
 			}
+			// If anything besides "/emsg" OR "/emsg reload" is entered.
 			else if(extras.length > 1 || !extras[0].equalsIgnoreCase("reload"))
 				sender.sendMessage(ChatColor.RED + "Incorrect arguments!");
 		}
