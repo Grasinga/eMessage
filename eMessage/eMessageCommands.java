f22c2a88950498c58ae9a1b56bb00b6cf42a45e8
Justin T. Brown <Grasinga@gmail.com>
2015-08-02 05:26:43 -0600

    Major Update 2
    Added commands to: change time delay, add messages, and remove messages
    all within the game.

--- eMessage/eMessageCommands.java
+++ eMessage/eMessageCommands.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import io.puharesource.mc.titlemanager.api.ActionbarTitleObject; // Import from TitleManager
 
@@ -40,8 +41,18 @@ public class eMessageCommands implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] extras) {
-		if (label.equalsIgnoreCase("emsg")){
+		if (label.equalsIgnoreCase("emsg")){			
 			if(extras.length <= 0){
+				
+				// If player just typed /emsg
+				if(sender instanceof Player){
+					sender.sendMessage(ChatColor.GOLD + "/emsg time <seconds> | Sets time delay between ads.");
+					sender.sendMessage(ChatColor.GOLD + "/emsg add <message> | Adds the advertisement.");
+					sender.sendMessage(ChatColor.GOLD + "/emsg remove <message> | Removes the advertisement.");
+					sender.sendMessage(ChatColor.GOLD + "/emsg reload | Reloads the plugin.");
+					return true;
+				}
+				
 				// /emsg creates a recurring advertisement. The text of the advertisements are found in the configuration file.
 				 ads = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
 		            @Override
@@ -63,21 +74,97 @@ public class eMessageCommands implements CommandExecutor {
 			     }, 0L, (20 * timeDelay)); // Number of seconds before next message appears.
 				 return true;
 			}
-			// Reload command /emsg reload
-			else if(extras.length == 1 && extras[0].equalsIgnoreCase("reload")) {
-				try{
+			else if(extras.length >= 1 && extras[0].equalsIgnoreCase("time")){
+				
+				// See if a number was entered.
+				try{Integer.parseInt(extras[1]);}
+				// An number wasn't entered.
+				catch(Exception e){sender.sendMessage(ChatColor.RED + "/emsg time <seconds>"); return false;}
+				
+				// Change and restart ads with new time delay.
+				if(Integer.parseInt(extras[1]) >= 0){
+					plugin.getConfig().set("TimeDelayBetweenMessages", Integer.parseInt(extras[1]));
+					plugin.saveConfig();
+					sender.sendMessage(ChatColor.GREEN + "Time between ads was changed to " + Integer.parseInt(extras[1]) + " seconds.");
 					plugin.reloadConfig();
 					timeDelay = plugin.getConfig().getInt("TimeDelayBetweenMessages");
+					Bukkit.getScheduler().cancelTask(ads);
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "emsg");
+					return true;
+				}
+				
+				// Number entered was less than 0.
+				else if(Integer.parseInt(extras[1]) < 0)
+					sender.sendMessage(ChatColor.RED + "Enter a number greater than -1!");
+				else
+					sender.sendMessage(ChatColor.RED + "/emsg time <seconds>");
+				return false;
+			}
+			// Add ads in-game.
+			else if(extras.length >= 1 && extras[0].equalsIgnoreCase("add")){
+				
+				if(extras.length == 1 || extras[1].toString().equals("")){
+					sender.sendMessage(ChatColor.RED + "/emsg add <messsage>");
+					return false;
+				}
+				
+				String newMessage = "";
+				for(int i=1;i<extras.length;i++)
+					newMessage += (extras[i].toString() + " ");
+				newMessage = newMessage.substring(0, newMessage.length() - 1);
+				messages.add(newMessage);
+				
+				plugin.getConfig().set("Messages", messages);
+				plugin.saveConfig();
+				sender.sendMessage(ChatColor.GREEN + "\"" + newMessage + "\" was added!");
+				plugin.reloadConfig();
+				messages = plugin.getConfig().getStringList("Messages");
+				msgAmount = messages.size();
+				Bukkit.getScheduler().cancelTask(ads);
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "emsg");
+				return true;
+			}
+			// Remove ads in-game.
+			else if(extras.length >= 1 && extras[0].equalsIgnoreCase("remove")){
+				
+				if(extras.length == 1){
+					sender.sendMessage(ChatColor.RED + "/emsg remove <messsage>");
+					return false;
+				}
+				
+				String removeMessage = "";
+				for(int i=1;i<extras.length;i++)
+					removeMessage += (extras[i].toString() + " ");
+				removeMessage = removeMessage.substring(0, removeMessage.length() - 1);
+				
+				if(messages.contains(removeMessage)){
+					messages.remove(removeMessage);
+					plugin.getConfig().set("Messages", messages);
+					plugin.saveConfig();
+					sender.sendMessage(ChatColor.GREEN + "\"" + removeMessage + "\" was removed!");
+					plugin.reloadConfig();
 					messages = plugin.getConfig().getStringList("Messages");
 					msgAmount = messages.size();
 					Bukkit.getScheduler().cancelTask(ads);
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "emsg");
-					sender.sendMessage(ChatColor.GREEN + "eMessage's configuration has been reloaded!");					
+					return true;
+				}
+				else{
+					sender.sendMessage(ChatColor.RED + "\"" + removeMessage + "\" was not found!");
+					return true;
+				}
+			}
+			// Reload command /emsg reload
+			else if(extras.length == 1 && extras[0].equalsIgnoreCase("reload")) {
+				try{
+					plugin.getServer().getPluginManager().disablePlugin(plugin);
+                    plugin.getServer().getPluginManager().enablePlugin(plugin);
+					sender.sendMessage(ChatColor.GREEN + "[eMessage] has been reloaded!");					
 				}catch(Exception e){sender.sendMessage(ChatColor.RED + "Error: " + e); return false;} // Fails to reload.
 				return true;
 			}
-			// If anything besides "/emsg" OR "/emsg reload" is entered.
-			else if(extras.length > 1 || !extras[0].equalsIgnoreCase("reload"))
+			// If anything besides the above commands are entered.
+			else
 				sender.sendMessage(ChatColor.RED + "Invalid arguments!");
 		}
 		return false;
